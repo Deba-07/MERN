57041eadc863d28303e506b49400d6832623d418
Deba-07 <dynamicdeba07@gmail.com>
2025-05-26 09:37:38 +0530

    while and do-while loops

---
+++ js essentials/05_iterations/02_while_do_while.js
@@ -0,0 +1,22 @@
+// while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(`Value of i: ${i}`);
+  i = i + 2;
+}
+
+let myArr = ["Optimus Prime", "Bumble Bee", "Ironhide", "Mirage"];
+let arr = 0;
+while (arr < myArr.length) {
+  console.log(myArr[arr]);
+  arr++;
+}
+
+// do-while loop
+
+let score = 1;
+do {
+  console.log(`Score is: ${score}`);
+  score++;
+} while (score <= 10);
