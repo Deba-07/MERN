1438e609422c5dd15f959d39c67b058962cbbe50
Deba-07 <dynamicdeba07@gmail.com>
2025-11-07 10:10:27 +0530

    feat: create events page with event fetching and display functionality

---
+++ nextJs/dev-event/app/events/page.tsx
@@ -0,0 +1,31 @@
+import EventCards from "@/components/EventCards";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
+    return (
+        <section>
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCards {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Page;
